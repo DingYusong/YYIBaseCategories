d467604c5a37146694d56e953135437d0b9b5ce2
丁玉松 <yusong.ding@huangjinqianbao.com>
2017-07-20 17:34:27 +0800

    Initial commit

---
+++ Example/YYIBaseCategories/YYIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YYIAppDelegate.h
+//  YYIBaseCategories
+//
+//  Created by DingYusong on 07/20/2017.
+//  Copyright (c) 2017 DingYusong. All rights reserved.
+//
+
+@import UIKit;
+
+@interface YYIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/YYIBaseCategories/YYIViewController.h
@@ -0,0 +1,13 @@
+//
+//  YYIViewController.h
+//  YYIBaseCategories
+//
+//  Created by DingYusong on 07/20/2017.
+//  Copyright (c) 2017 DingYusong. All rights reserved.
+//
+
+@import UIKit;
+
+@interface YYIViewController : UIViewController
+
+@end
